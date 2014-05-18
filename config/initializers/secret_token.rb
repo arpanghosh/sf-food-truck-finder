29709b47addff55ebba10d373e27ee63eeb54b77
Arpan Ghosh <arpanghosh@gmail.com>
2014-05-17 22:25:34 -0700

    Initial commit :Initialized Rails 4.0.1 project. Added soda-ruby gem to Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FoodTruckFinder::Application.config.secret_key_base = 'bf31ed3c2c75f37a2547dc514fd8f41b0dbbddd8707738acdb1eebb1ff85df1f7b393232cb70082b685943c14de2d3696279ded003a64bcc77e8fe121f1d7e0a'
